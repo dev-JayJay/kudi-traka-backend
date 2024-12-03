cf07def404fdfa9957948a3a99362a09c1bbc4d1
dev-jayjay <johngratia2@gmail.com>
2024-12-03 13:51:17 +0100

    setting only port 3000 on cross

--- api/server.js
+++ api/server.js
@@ -61,7 +61,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'https://kudi-traka.vercel.app'], // Allow both origins
+    origin: 'http://localhost:3000', // Allow both origins https://kudi-traka.vercel.app
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'], 
   },
@@ -70,7 +70,7 @@ require("dotenv").config();
 
 // Middleware
 app.use(cors({ 
-  origin: ["http://localhost:3000", "https://kudi-traka.vercel.app"] 
+  origin: "http://localhost:3000", 
 }));
 app.use(bodyParser.json());
 
