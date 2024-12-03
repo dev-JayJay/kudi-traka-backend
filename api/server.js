2d9e514099c5c1bd597e7a15ce104c9b0343064c
dev-jayjay <johngratia2@gmail.com>
2024-12-03 14:36:24 +0100

    added root

--- api/server.js
+++ api/server.js
@@ -182,6 +182,15 @@ io.on("connection", (socket) => {
   });
 });
 
+app.get('/', (req, res) => {
+  res.status(200).send('server is runing on forward slash and set mongo IP to anywhere');
+  console.log(`checking the check`);
+})
+app.get('/check', (req, res) => {
+  res.status(200).send('server is runing on this url');
+  console.log(`checking the check`);
+})
+
 server.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
 });
